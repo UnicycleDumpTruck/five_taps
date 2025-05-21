9b88dec6df58cc0d48b2e30510ac8bca1a972181
Jeff <junk@jeffhighsmith.com>
2025-05-21 13:40:12 -0400

    Single coin reading, placeholders for more

--- jumpcoin.py
+++ jumpcoin.py
@@ -1,13 +1,11 @@
 import device_model
 import time
 import threading
-import sounddevice as sd
-import soundfile as sf
 import pyglet
 
-MIN_SECONDS_BETWEEN_SOUNDS = 1.0 # Fractional seconds before a coin can play another sound
-
-############## JumpCoin
+MIN_SECONDS_BETWEEN_SOUNDS = (
+    1.0  # Fractional seconds before a coin can play another sound
+)
 
 # For now, we'll keep the JumpCoin's separate, each to be run in a separate thread
 # We could put them all on the same modbus and address them there,
@@ -15,41 +13,30 @@ MIN_SECONDS_BETWEEN_SOUNDS = 1.0 # Fractional seconds before a coin can play ano
 # if each gets its own modbus and USB port and the computer uses
 # one thread for each USB port.
 
+
 class JumpCoin:
     # Data update event
     def updateData(self, DeviceModel):
-        acc_z = DeviceModel.deviceData[self.modbus_addr]['AccZ']
+        acc_z = DeviceModel.deviceData[self.modbus_addr]["AccZ"]
         if acc_z < -1.015:
-            if self.playing_sound == False:
-                self.playsound()
+            self.playsound()
             print("Acceleration in Z-Axis measured", acc_z)
-
-
 
     def __init__(self, coin_name, serial_port, baudrate, modbus_addr, audio_file):
         self.coin_name = coin_name
         self.serial_port = serial_port
         self.baudrate = baudrate
         self.modbus_addr = modbus_addr
-        self.audio_file = audio_file
-        self.data, self.samplerate = sf.read(self.audio_file)
-        self.playing_sound = False
         self.time_last_sound = time.monotonic()
-        
-        # Pyglet Sound
-        self.sound_player = pyglet.resource.media(self.audio_file, streaming=False)
-        #sound.play()
-
-        #self.loaded_sound = pyglet.media.load(audio_file)
-        #self.sound_player = pyglet.media.Player()
-        #self.sound_player.queue(self.loaded_sound)
+        self.sound_player = pyglet.resource.media(audio_file, streaming=False)
 
         self.accelerometer = device_model.DeviceModel(
-                self.coin_name,
-                self.serial_port,
-                self.baudrate,
-                [self.modbus_addr],
-                self.updateData)
+            self.coin_name,
+            self.serial_port,
+            self.baudrate,
+            [self.modbus_addr],
+            self.updateData,
+        )
         self.accelerometer.openDevice()
         self.accelerometer.startLoopRead()
 
@@ -58,13 +45,10 @@ class JumpCoin:
         t.start()
 
     def playsound(self):
-        self.sound_playing = True
         current_time = time.monotonic()
-        if ((current_time - self.time_last_sound) > MIN_SECONDS_BETWEEN_SOUNDS):
+        if (current_time - self.time_last_sound) > MIN_SECONDS_BETWEEN_SOUNDS:
             self.time_last_sound = time.monotonic()
             print("Kaching")
             self.sound_player.play()
         else:
             print(f"Too soon for another sound from {self.coin_name}")
-            print(f"Current: {current_time} Last: {self.time_last_sound}")
-
--- main.py
+++ main.py
@@ -1,12 +1,60 @@
 import jumpcoin
 import pyglet
+
+dime = {
+    "coin_name": "Dime",
+    "serial_port": "/dev/ttyUSB0",
+    "baudrate": 9600,
+    "modbus_addr": 0x50,
+    "audio_file": "kaching.wav",
+}
+pizza_penny = {
+    "coin_name": "Pizza Penny",
+    "serial_port": "/dev/ttyUSB0",
+    "baudrate": 9600,
+    "modbus_addr": 0x50,
+    "audio_file": "kaching.wav",
+}
+truck_penny = {
+    "coin_name": "Truck Penny",
+    "serial_port": "/dev/ttyUSB0",
+    "baudrate": 9600,
+    "modbus_addr": 0x50,
+    "audio_file": "kaching.wav",
+}
+nickel = {
+    "coin_name": "Nickel",
+    "serial_port": "/dev/ttyUSB0",
+    "baudrate": 9600,
+    "modbus_addr": 0x50,
+    "audio_file": "kaching.wav",
+}
+quarter = {
+    "coin_name": "Quarter",
+    "serial_port": "/dev/ttyUSB0",
+    "baudrate": 9600,
+    "modbus_addr": 0x50,
+    "audio_file": "kaching.wav",
+}
+
+coin_data = [
+    dime,
+    #pizza_penny,
+    #truck_penny,
+    #nickel,
+    #quarter,
+]
+coins = []
 if __name__ == "__main__":
-    dime = jumpcoin.JumpCoin(
-            "Dime",
-            "/dev/ttyUSB0",
-            9600,
-            0x50,
-            "kaching.wav",
+    for coin_datum in coin_data:
+        coins.append(
+            jumpcoin.JumpCoin(
+                coin_datum["coin_name"],
+                coin_datum["serial_port"],
+                coin_datum["baudrate"],
+                coin_datum["modbus_addr"],
+                coin_datum["audio_file"],
             )
-    pyglet.app.run()
+        )
     print("Done initializing coins in main.py")
+    pyglet.app.run()
